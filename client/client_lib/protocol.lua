2f5369f1a56c68c226af573fd113b423f2f25470
thelastshine <notshiny09@gmail.com>
2026-08-03 19:46:37 -0300

    Update protocol.lua

--- client/client_lib/protocol.lua
+++ client/client_lib/protocol.lua
@@ -6,8 +6,8 @@
     servers on the same in-game network without them interfering).
 
     IMPORTANT: This value must be IDENTICAL to the PROTO_SUFFIX set in
-    server/server_lib/protocol.lua on the server computer, or the
-    client and server will not be able to talk to each other.
+    client/client_lib/protocol.lua on every client computer, or the
+    server and clients will not be able to talk to each other.
 ]]
 
 local PROTO_SUFFIX = "2"
@@ -19,20 +19,20 @@ end
 return {
     SUFFIX = PROTO_SUFFIX,
 
-    SERVER        = proto("PROTO_SERVER"),
-    SERVER_REPLY  = proto("PROTO_SERVER") .. ":REPLY",
-    SERVER_STATE  = proto("PROTO_SERVER_STATE"),
-    SERVER_QUEUE  = proto("PROTO_SERVER_QUEUE"),
-    SERVER_PLAYER = proto("PROTO_SERVER_PLAYER"),
+    SERVER        = proto("PROTO_SERVER2"),
+    SERVER_REPLY  = proto("PROTO_SERVER2") .. ":REPLY",
+    SERVER_STATE  = proto("PROTO_SERVER_STATE2"),
+    SERVER_QUEUE  = proto("PROTO_SERVER_QUEUE2"),
+    SERVER_PLAYER = proto("PROTO_SERVER_PLAYER2"),
 
-    AUDIO            = proto("PROTO_AUDIO"),
-    AUDIO_NEXT       = proto("PROTO_AUDIO_NEXT"),
-    AUDIO_HALT       = proto("PROTO_AUDIO_HALT"),
-    AUDIO_CONNECTION = proto("PROTO_AUDIO_CONNECTION"),
-    AUDIO_STATUS     = proto("PROTO_AUDIO_STATUS"),
+    AUDIO            = proto("PROTO_AUDIO2"),
+    AUDIO_NEXT       = proto("PROTO_AUDIO_NEXT2"),
+    AUDIO_HALT       = proto("PROTO_AUDIO_HALT2"),
+    AUDIO_CONNECTION = proto("PROTO_AUDIO_CONNECTION2"),
+    AUDIO_STATUS     = proto("PROTO_AUDIO_STATUS2"),
 
-    COMMAND     = proto("PROTO_COMMAND"),
-    CLIENT_SYNC = proto("PROTO_CLIENT_SYNC"),
-    UPDATED     = proto("PROTO_UPDATED"),
-    CHATBOX     = proto("PROTO_CHATBOX"),
+    COMMAND     = proto("PROTO_COMMAND2"),
+    CLIENT_SYNC = proto("PROTO_CLIENT_SYNC2"),
+    UPDATED     = proto("PROTO_UPDATED2"),
+    CHATBOX     = proto("PROTO_CHATBOX2"),
 }
